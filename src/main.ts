6026e2a1a9febb00922af74935071d7bffa188b9
mccartymv <mvmccart@gmail.com>
2024-06-30 19:43:21 -0400

    install angular material

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient()  // Provide HttpClient here
+    provideHttpClient(), provideAnimationsAsync()  // Provide HttpClient here
   ]
 })
 .catch(err => console.error(err));
