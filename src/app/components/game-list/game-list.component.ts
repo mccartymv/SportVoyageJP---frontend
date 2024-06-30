7daa3dee319a4be07c03a421228ab52b27c7dd51
mccartymv <mvmccart@gmail.com>
2024-06-30 17:30:03 -0400

    add ag-grid native filtering on right sidebar

--- src/app/components/game-list/game-list.component.ts
+++ src/app/components/game-list/game-list.component.ts
@@ -3,7 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { AgGridModule } from 'ag-grid-angular';
 import { GameService } from '../../services/game.service';
-import { ColDef } from 'ag-grid-community';
+import { ColDef, GridApi } from 'ag-grid-community';
 
 @Component({
   selector: 'app-game-list',
@@ -13,29 +13,61 @@ import { ColDef } from 'ag-grid-community';
   imports: [CommonModule, HttpClientModule, AgGridModule]
 })
 export class GameListComponent implements OnInit {
+  private gridApi!: GridApi;
   games: any[] = [];
+  filteredGames: any[] = [];
+  teams: string[] = [];
+  selectedTeams: Set<string> = new Set();
+
   columnDefs: ColDef[] = [
-    { field: 'gameId', headerName: 'Game ID' },
-    { field: 'startTime', headerName: 'Start Time', valueFormatter: this.dateFormatter },
-    { field: 'homeTeam', headerName: 'Home Team' },
-    { field: 'awayTeam', headerName: 'Away Team' },
-    { field: 'venue.name', headerName: 'Venue Name' },
-    { field: 'venue.city', headerName: 'Venue City' }
+    { field: 'gameId', headerName: 'Game ID', filter: 'agTextColumnFilter' },
+    { field: 'startTime', headerName: 'Start Time', filter: 'agDateColumnFilter', valueFormatter: this.dateFormatter },
+    { field: 'homeTeam', headerName: 'Home Team', filter: 'agTextColumnFilter' },
+    { field: 'awayTeam', headerName: 'Away Team', filter: 'agTextColumnFilter' },
+    { field: 'venue.name', headerName: 'Venue Name', filter: 'agTextColumnFilter' },
+    { field: 'venue.city', headerName: 'Venue City', filter: 'agTextColumnFilter' }
   ];
   defaultColDef: ColDef = {
     sortable: true,
     filter: true,
   };
 
-  constructor(private gameService: GameService) { }
+  constructor(private gameService: GameService) {}
 
   ngOnInit(): void {
     this.gameService.getGames().subscribe((data) => {
-      this.games = data.map(game => ({
+      this.games = data.map((game) => ({
         ...game,
         startTime: new Date(game.startTime)
       }));
+      this.filteredGames = this.games;
+      this.teams = Array.from(new Set(this.games.flatMap(game => [game.homeTeam, game.awayTeam])));
     });
+  }
+
+  onGridReady(params: any) {
+    this.gridApi = params.api;
+  }
+
+  onTeamSelectionChange(event: any) {
+    const team = event.target.value;
+    if (event.target.checked) {
+      this.selectedTeams.add(team);
+    } else {
+      this.selectedTeams.delete(team);
+    }
+    this.applyTeamFilter();
+  }
+
+  applyTeamFilter() {
+    if (this.selectedTeams.size === 0) {
+      this.filteredGames = this.games;
+    } else {
+      this.filteredGames = this.games.filter(game =>
+        this.selectedTeams.has(game.homeTeam) || this.selectedTeams.has(game.awayTeam)
+      );
+    }
+    this.gridApi.setRowData(this.filteredGames);
   }
 
   dateFormatter(params: any): string {
